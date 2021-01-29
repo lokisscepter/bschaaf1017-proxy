4168d54a05bbb51a5e71c45d459874a2d0fc44c1
Brian <bschaaf1017@gmail.com>
2021-01-29 15:29:35 -0500

    proxy

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(cors());
 app.use(express.static('public'));
 
-app.use(createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
+app.use(createProxyMiddleware({ target: 'http://3.132.120.26:8080', changeOrigin: true }));
 app.listen(3030);
