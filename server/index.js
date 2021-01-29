1ffe3e6b90897a7611802e77a977c42c468aa8c5
Brian <bschaaf1017@gmail.com>
2021-01-29 18:13:21 -0500

    change to use just express instead of proxy middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const PORT = 4040
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 
-app.use(createProxyMiddleware({ target: 'http://3.132.120.26:8080', changeOrigin: true }));
-app.listen(3030);
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT);
+});
